4e7e36dfb3b3be74646c0f724b9c06f941f9a922
siwat <siwatarm2013@gmail.com>
2018-03-22 16:15:31 +0700

    master
    update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,10 @@
 <?php 
-$access_token = 'AiAKn8D2SwU5WmMIdAz61xISQU1My3I7zCI131sHF0mOdcRIZWVEIpCBYb3qx31fB/DylvOS+qW+osZW1N+SVWOeiGzlPUGNRVBKRk6CQhX54OHp/qc+ge/bORapsnMlD3p+hD2/nNQzlkvtQxJdEgdB04t89/1O/w1cDnyilFU=
-';
-
-'https://api.line.me/v1/oauth/verify';$headers = array('Authorization: Bearer ' . $access_token);$ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);$result = curl_exec($ch);curl_close($ch);echo $result;
+$access_token ='uW96In3bdMmPl+UZKY93eBfsEZNgvhl6TluiMycGdAMIIMSJZ079VdWtGmN1fOW8B/DylvOS+qW+osZW1N+SVWOeiGzlPUGNRVBKRk6CQhXjsWBbWji+zcqAF/rfuJKxunR92s8q5MuEr+z4jSfvcwdB04t89/1O/w1cDnyilFU=';
+'https://api.line.me/v1/oauth/verify';$headers = array('Authorization: Bearer '.$access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);curl_close($ch);
+echo $result;
 ?>
