a09bb16d0f15e17a8725462a93f447154a39891a
siwat <siwatarm2013@gmail.com>
2018-03-22 16:58:25 +0700

    master

--- verify.php
+++ verify.php
@@ -1,6 +1,6 @@
 <?php 
 $access_token = 'EunneITz+CDcCPDQVRAAUNVyOKRMj1Fwp66SwZpQOpKtyYcbcUfxQLyFrEplCzgIB/DylvOS+qW+osZW1N+SVWOeiGzlPUGNRVBKRk6CQhXt+hD0MrbetZ9evG+RcZAepHRZty7xtNW12M2iUcNGEgdB04t89/1O/w1cDnyilFU=';
-$url = 'https://api.line.me/v2/oauth/verify';
+$url = 'https://api.line.me/oauth2/v2.1/verify';
 $headers = array('Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
