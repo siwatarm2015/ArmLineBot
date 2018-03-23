26c0aa1274be1478e9e1a22310a3af9ffd38c775
siwat <siwatarm2013@gmail.com>
2018-03-23 17:41:30 +0700

    master

--- bot.php
+++ bot.php
@@ -22,7 +22,7 @@ $text = $event['message']['text'];
 // Get replyToken			
 $replyToken = $event['replyToken'];			
 // Build message to reply back			
-$messages = [				'type' => 'text',				'text' => $text			];			
+$messages = ['type' => 'text','text' => $text];			
 // Make a POST Request to Messaging API to reply to sender			
 $url = 'https://api.line.me/v2/bot/message/reply';			
 $data = ['replyToken' => $replyToken,'messages' => [$messages],];			
