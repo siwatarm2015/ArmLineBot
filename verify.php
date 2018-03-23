856f84568efcc0757576c998c535d0e138547f42
siwat <siwatarm2013@gmail.com>
2018-03-23 13:30:18 +0700

    master

--- verify.php
+++ verify.php
@@ -1,9 +1,10 @@
 <?php 
 $access_token = 'KpFfecrkMgy+VLpBQqBeIZb+PqzvlGwmUlWQj738u8jczoc8iSPxmbegJMq9EZK0B/DylvOS+qW+osZW1N+SVWOeiGzlPUGNRVBKRk6CQhVi39+t7fQksFdJL4vOCgHUYWzN/2AsgdkuPNzEgBqFSQdB04t89/1O/w1cDnyilFU=';
-$url = 'https://api.line.me/v1/oauth/verify';
-$headers = array('Authorization: Bearer '.$access_token);
+$url = 'https://api.line.me/v2/oauth/verify?access_token='.$access_token;
+$headers = array('Content-Type: application/x-www-form-urlencoded');
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+CURLOPT_POST => true,
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);curl_close($ch);
